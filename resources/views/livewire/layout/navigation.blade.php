e3b8bbb501d3a62a3f401df257bc15a42fdb4673
AimeePerryVS <aimee.perry@visualsoft.co.uk>
2024-05-08 08:28:21 +0100

    work from day 1

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -33,12 +33,13 @@ new class extends Component
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                <x-nav-link :href="route('chirps')" :active="request()->routeIs('chirps')" wire:navigate>
-                    {{ __('Chirps') }}
-                </x-nav-link>
-                    <x-nav-link :href="route('users')" :active="request()->routeIs('users')" wire:navigate>
-                        {{ __('users') }}
+                    <x-nav-link :href="route('chirps')" :active="request()->routeIs('chirps')" wire:navigate>
+                        {{ __('Chirps') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('users')" :active="request()->routeIs('users')" wire:navigate>
+                        {{ __('Users') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
